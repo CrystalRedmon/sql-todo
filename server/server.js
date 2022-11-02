a2c0f13242c6d5f1def5f234187cc8ac06b6f85a
Crystal Redmon <cato2848@gmail.com>
2022-11-02 16:08:29 -0500

    change pool host

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config();  /// required to be able to load dotenv file
 const express = require('express');
 const bodyParser = require('body-parser');
 const pool = require('./modules/pool');
@@ -76,9 +76,61 @@ app.put('/tasks/:id', (req, res) => {
     });
 });
 
-const port = 3000;
+// const port = 3000;
+
+const port = process.env.PORT || 3000;
+
+
 
 console.log(`We're going to listen on port`, port);
 app.listen(port, () => {
   console.log(`Listening on ${port}`);
 })
+
+// FAV_FOOD=artichokes PORT=8000  npm start 
+//process.env will allow you to change info specific to your computer
+// const port = process.env.PORT || 3000;
+// console.log('process.env', process.env);
+
+// console.log("Favorite food", process.env.FAV_FOOD);
+
+
+// environment variables 
+// variables set on the computer not on the code. It belongs to the computer not just for a particular app.
+// Whoever is running the code can set the variable
+
+//dotenv  -->>> npm install dotenv
+// create .env file
+
+
+// More instances will cost more. Only one instance is free
+
+
+
+// AWS
+// launch instance, connect
+// make sure local version is commited and pushed
+// go to individual github URL
+// copy http code 
+// git clone HTTPcodefromGitHub
+// cd into file
+// input-  npm install
+// ----->>>>   .env is not on AWS
+
+// input: vim .env
+// input:  cat .env
+// input: pm2 start server/server.js  
+
+// pm2 keeps server/server.js running even if window closes
+
+// AWS in search rds (relational database service)
+// create DB
+// default- postgresSQL-free tier
+
+// lalaNow80!
+
+
+// public access yes
+//vpc security group- create new
+// name security group
+// create DB
